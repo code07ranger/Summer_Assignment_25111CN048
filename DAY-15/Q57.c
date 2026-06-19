6bf9466881b6c086c4c7fc95850b1b8cfe0f9880
code07ranger <harshmahawar47@gmail.com>
2026-06-19 12:19:51 +0530

    Add files via upload

---
+++ DAY-15/Q57.c
@@ -0,0 +1,25 @@
+//Write a program to Reverse array.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter element of array: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int i=0,j=n-1;
+    while(i<j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        i++;
+        j--;
+    }
+    printf("reverse array: ");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
